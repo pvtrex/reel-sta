dd508074b195a2e6f31013bb99a3e33d9f72b837
pvtrex <princegangwar077@gmail.com>
2026-05-06 17:04:01 +0530

    feat: implement root layout with global providers and header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,11 @@ import Header from "./components/Header";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "InstaPro - ImageKit Next.js Integration",
+  title: "Instapro - ImageKit Next.js Integration",
   description: "Demo of ImageKit integration with Next.js",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 
